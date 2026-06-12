0d1ab114ae6780763f0e39aea20a79b4b86ac8cb
ManharCK <manhargurukkal2@gmail.com>
2026-06-12 18:36:27 +0530

    updated

---
+++ task.py
@@ -0,0 +1,77 @@
+text="manhar gurukkal ck"
+
+dup=""
+secdup=""
+
+for i in text:
+    if i not in dup:
+        dup+=i
+    else: 
+        
+        secdup+=i
+        
+# print(secdup)
+# print(dup)
+
+print(secdup[1])
+
+# def decor(func):
+#     def wrapper():
+#         return func().upper()
+#     return wrapper
+        
+# @decor
+# def one():
+#     return "manhar gurukkal ck"
+    
+# print(one())
+
+
+class A:
+    def one(self):
+         print("a")
+
+class B(A):
+    def one(self):
+        print("b")
+        
+    
+class C(B):
+    def one(self):
+        print("c")
+        
+
+class D(B,C):
+    pass
+a=D()
+a.one()
+
+
+text="manhar gurukkal ck"
+
+dup=""
+secdup=""
+
+for i in text:
+    if i not in dup:
+        dup+=i
+    else: 
+        
+        secdup+=i
+        
+# print(secdup)
+# print(dup)
+
+print(secdup[1])
+
+
+
+
+
+
+    
+        
+
+
+
+    
